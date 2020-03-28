d95412a4f13599fb25565249c0757c8025bd6cc9
mariollorente <mariollorente@localhost>
2020-03-28 17:01:11 +0100

    ficheros

---
+++ escribir_fichero.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define MAX_NUM 10
+
+int main(void)
+{
+
+	FILE* f;
+        int c;
+
+	//abrir fichero para escritura "w"
+	f = fopen("prueba.txt", "w");
+
+	//escribir en fichero un string formateado
+	fprintf(f, "Escribiendo %i números\n", MAX_NUM);
+	for (c = 0; c < MAX_NUM; c++)
+		fprintf(f, "%i\n", c);
+
+	//cerrar fichero
+	fclose(f);
+
+	return 0;
+}
