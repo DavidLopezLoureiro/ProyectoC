bf381a74308d466867c71c55f6806883650a8a4e
mariollorente <mariollorente@localhost>
2020-03-28 16:53:00 +0100

    menu main, 2 modulos

--- main.c
+++ main.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "Aula.h"
 #include "Alumno.h"
 #include "Profesor.h"
 #include "Asignatura.h"
+
+
+#define MAX_LINE
+
+
+
+void clearIfNeeded(char *str, int max_line)
+	{
+		// Limpia los caracteres de m·s introducidos
+		if ((strlen(str) == max_line-1) && (str[max_line-2] != '\n'))
+			while (getchar() != '\n');
+	}
+
+	char mostrarMenu()
+	{
+		printf("1. Introducir asignatura en aula \n");
+		printf("2. \n");
+		printf("Pulsar 'q' para salir\n");
+		printf("\n");
+		printf("Opcion: ");
+		fflush(stdout);
+		char linea[MAX_LINE];
+		fgets(linea, MAX_LINE, stdin);
+		clearIfNeeded(linea, MAX_LINE);
+		return * linea;
+}
+
+
 
 int main() {
 
@@ -33,4 +62,7 @@ int main() {
 
 	return 0;
 
-}
+
+
+	}
+
